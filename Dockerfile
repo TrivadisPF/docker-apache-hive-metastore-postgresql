6fb092f1166f943f0a46c190c30ac278a34396e0
Guido Schmutz <schmutz68@gmail.com>
2024-10-15 15:28:16 +0200

    add HIVE_USER and HIVE_PASSWORD environment variable to support configuring it

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM postgres:9.5.3
 
 MAINTAINER "Guido Schmutz <guido.schmutz@trivadis.com>"
 
+ENV HIVE_USER=hive
+ENV HIVE_PASSWORD=hive
+
 COPY hive-schema-2.3.0.postgres.sql /hive/hive-schema-2.3.0.postgres.sql
 COPY hive-txn-schema-2.3.0.postgres.sql /hive/hive-txn-schema-2.3.0.postgres.sql
 COPY upgrade-2.3.0-to-3.0.0.postgres.sql /hive/upgrade-2.3.0-to-3.0.0.postgres.sql
--- init-hive-db.sh
+++ init-hive-db.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
+# Ensure the HIVE_PASSWORD environment variable is set
+if [ -z "$HIVE_PASSWORD" ]; then
+  echo "HIVE_PASSWORD environment variable is not set."
+  exit 1
+fi
+
+# Ensure the HIVE_USER environment variable is set
+if [ -z "$HIVE_USER" ]; then
+  echo "HIVE_USER environment variable is not set."
+  exit 1
+fi
+
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE USER hive WITH PASSWORD 'hive';
+  CREATE USER '$HIVE_USER' WITH PASSWORD '$HIVE_PASSWORD';
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
 
